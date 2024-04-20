531250928e447578ad81d8b939fb2e9fe5153207
sangw0n <pilwon070127@naver.com>
2024-04-21 04:08:02 +0900

    Player Run Animation

--- Assets/01. Scripts/Player/PlayerMoveCtrl.cs
+++ Assets/01. Scripts/Player/PlayerMoveCtrl.cs
@@ -6,21 +6,48 @@ public class PlayerMoveCtrl : MonoBehaviour
 {
     [Header("[# Move Var Header]")]
     [SerializeField] private int moveSpeed;
-    [SerializeField] private Vector2 moveVec;
+    private float inputMoveDir;
+    private Vector2 moveVec;
+
+    // Object :: Component
+    private Animator anim;
+
+    // Animator :: Hash Value
+    private readonly int hashIsMove = Animator.StringToHash("isMove");
+    private readonly int hashMove = Animator.StringToHash("Move");
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
 
     private void Update()
     {
         Move();
     }
 
+    private void LateUpdate()
+    {
+        // Animator :: 설정 
+
+        // inputMoveDir 값이 0과 거의 동일하면 애니메이션 비활성화
+        if (Mathf.Approximately(inputMoveDir, 0)) anim.SetBool(hashIsMove, false);
+        // inputMoveDir 값이 0과 거의 동일하지 않으면 애니메이션 활성화
+        else anim.SetBool(hashIsMove, true);
+
+        // 이동 방향에 따라 애니메이터의 MoveVec 파라미터 값을 설정
+        anim.SetFloat(hashMove, inputMoveDir);
+    }
+
     private void Move()
     {
         // 키를 눌러서 방향 받아오기
-        float dirVec = Input.GetAxis("Horizontal");
-        if(dirVec == 0) return;
+        inputMoveDir = Input.GetAxis("Horizontal");
+        // 입력 값이 없으면 실행하지 않고 리턴
+        if (inputMoveDir == 0) return;
 
         // 입력받은 방향으로 이동하기
-        moveVec.x = dirVec * moveSpeed * Time.deltaTime;
+        moveVec.x = inputMoveDir * moveSpeed * Time.deltaTime;
         transform.Translate(moveVec);
     }
 }
