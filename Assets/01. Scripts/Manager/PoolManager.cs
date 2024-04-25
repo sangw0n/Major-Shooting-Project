ee45c8023b24d3056a7ba062bd21a178f3f2bc02
sangw0n <pilwon070127@naver.com>
2024-04-25 23:42:47 +0900

    PoolManager Feat
    풀 매니저 기능 추가

--- Assets/01. Scripts/Manager/PoolManager.cs
+++ Assets/01. Scripts/Manager/PoolManager.cs
@@ -1,14 +1,35 @@
+// # System
+using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
-// 다중 오브젝트 풀링
+// # Unity 
+using UnityEngine;
+using UnityEngine.Pool;
+
+// 오브젝트 풀의 정보를 저장할 클래스 
+[System.Serializable]
+public class ObjectPoolData
+{
+    public string keyName; // 키 이름 
+    public GameObject prefab; // 오브젝트 프리팹
+    public int initObejctCount; // 오브젝트 초기 생성 개수
+}
+
+// 다중 오브젝트 풀링을 관리하는 클래스 
 public class PoolManager : MonoBehaviour
 {
+    // PoolManager 클래스의 싱글톤 인스턴스
     public static PoolManager Instance { get; private set; }
+
+    // 각 오브젝트 풀의 "키 이름", "프리팹", "오브젝트 생성 개수"를 관리하는 배열 변수 
+    [SerializeField] private ObjectPoolData[] objectPoolDatas;
+
+    // 생성된 오브젝트 풀들을 저장할 딕셔너리 
+    private Dictionary<string, Queue<GameObject>> objectPools = new Dictionary<string, Queue<GameObject>>();
 
     private void Awake()
     {
-        if(Instance == null) Instance = this;
+        if(Instance == null) Instance = this;   
     }
 }
