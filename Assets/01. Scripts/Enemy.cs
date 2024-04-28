e4c9a51c70c25f0d287cddb8de428a065efa1762
sangw0n <pilwon070127@naver.com>
2024-04-29 01:17:34 +0900

    [Feat] Enemy Die  & Bullet ApplyDamage

--- Assets/01. Scripts/Enemy.cs
+++ Assets/01. Scripts/Enemy.cs
@@ -1,13 +1,26 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] private int enemyHp;
+    [SerializeField] private ObjecyKeyType keyType;
+
     private void OnCollisionEnter2D(Collision2D coll)
     {
         if (coll.gameObject.CompareTag("BULLET"))
         {
             // 풀로 리턴
-            PoolManager.Instance.ReturnObject(coll.gameObject, "BULLET");
+            PoolManager.Instance.ReturnObject(coll.gameObject, ObjecyKeyType.BULLET);
+            // 데미지 받음
+            enemyHp -= coll.gameObject.GetComponent<Bullet>().ApplyDamage();
+
+            // 체력을 전부 소모해 사망했다면
+            if (enemyHp <= 0)
+            {
+                // 풀로 다시 리턴
+                PoolManager.Instance.ReturnObject(this.gameObject, keyType);
+            }
         }
     }
 }
