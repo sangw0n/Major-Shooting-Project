6d3eeb4d385130f3845a10b53afa1ba743e9609b
sangw0n <pilwon070127@naver.com>
2024-05-04 22:23:42 +0900

    [Feat] Barrel Explosion

---
+++ Assets/01. Scripts/Barrel.cs
@@ -0,0 +1,49 @@
+// # Unity
+using System.Collections;
+using MajorProject;
+using UnityEngine;
+
+public class Barrel : MonoBehaviour
+{
+    [SerializeField] private int hitCount = 5;
+    [SerializeField] private float force;
+    [SerializeField] private GameObject explosionEffect;
+    [SerializeField] private Vector3 effectPosOffset;
+
+    private new Collider2D collider;
+    private Rigidbody2D rigid;
+
+    private void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        collider = GetComponent<Collider2D>();
+    }
+
+    // private void Explode()
+    // {
+    //     rigid.constraints = RigidbodyConstraints2D.None;
+    //     collider.enabled = false;
+
+    //     rigid.AddForce(Vector3.up * force, ForceMode2D.Impulse);
+    // }
+
+    private IEnumerator OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.CompareTag("BULLET"))
+        {
+            PoolManager.Instance.ReturnObject(coll.gameObject, ObjecyKeyType.BULLET);
+
+            if (--hitCount <= 0)
+            {
+                // 폭발하는 함수 
+                Destroy(this.gameObject);
+                // 폭발 이펙트 생성
+                GameObject effect = Instantiate(explosionEffect, transform.position + effectPosOffset, Quaternion.identity);
+                // 일정시간 후 폭발 이펙트 삭제
+                yield return new WaitForSeconds(0.5f);
+                Destroy(effect);
+            }
+        }
+    }
+}
+
